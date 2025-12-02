64cf40142b10f45d6366b47828399b48ed408426
romaingueffier <r.gueffier.pro@gmail.com>
2025-12-02 19:38:46 +0100

    feat: create theme palettes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
+import { Pixelify_Sans } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const font = Pixelify_Sans({
+  weight: '400',
   subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: '--font-sans',
 })
 
 export const metadata: Metadata = {
@@ -24,10 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${font.variable} antialiased font-sans`}>
+        <header className="bg-vintage-800 border-b-4 border-vintage-900 p-6">
+          <nav>
+            <ul className="flex items-center justify-center">
+              <li>
+                <Link
+                  className="bg-vintage-300 p-2 aspect-square border-4 border-vintage-800 hover:bg-vintage-400 active:translate-y-0.5 active:shadow-inner transition-all duration-100"
+                  href="/"
+                >
+                  🏠
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="bg-vintage-200">{children}</main>
+        <footer className="grid items-center p-4 justify-center text-xs bg-vintage-800 text-vintage-500">
+          Copyrights @RomainGueffier {new Date().getFullYear()}
+        </footer>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Vintage Story World Edit Models
-          </h1>
-          <p className="max-w-md text-xl leading-8 text-zinc-600 dark:text-zinc-400">
-            In construction... Available soon
-          </p>
-        </div>
-      </main>
+    <div className="text-vintage-900">
+      <h1 className="text-5xl font-semibold leading-10 tracking-widest uppercase text-center px-4 py-20">
+        Vintage Story World Edit Models
+      </h1>
+      <p className="p-8 text-lg text-center">
+        Here you can find JSON models of Vintage Story's Word Edit tool to share
+        structures between you worlds.
+      </p>
+      <p className="p-8 text-lg text-center">
+        In construction... Available soon
+      </p>
     </div>
   )
 }
